eef63e30065884e7eefeadc734b8871a381a4478
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-24 22:26:21 +0000

    Add project files.

---
+++ CaesarCipher/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaesarCipher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static char Encrypt(int shift, char character)
+        {
+            int number = GetNumber(character);
+            if (number == -1)
+                return character;
+            number = (number + shift) % 26;
+            return GetLetter(number);
+        }
+
+        private static char GetLetter(int number)
+        {
+            return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[number];
+        }
+
+        private static int GetNumber(char character)
+        {
+            character = Char.ToUpper(character);
+            return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".IndexOf(character);
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            EncryptedTextTextBox.Clear();
+            int shift = int.Parse(ShiftTextBox.Text);
+            foreach(char letter in PlainTextTextBox.Text)
+            {
+                EncryptedTextTextBox.AppendText(Encrypt(shift,letter).ToString());
+            }
+        }
+    }
+}
